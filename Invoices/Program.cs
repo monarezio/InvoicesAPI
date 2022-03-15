737b6e51149554a70a58f0b10b3a27631e424f38
samuel.kodytek <samuel.kodytek@guided.cz>
2022-03-15 11:42:20 +0100

    dsadassd

--- Invoices/Program.cs
+++ Invoices/Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
 }));
 
 var app = builder.Build();
+app.UseCors("MyPolicy");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
